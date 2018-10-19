23e9ccf19e6713579114734e06f22e651d211694
Joe Amedeo <joseph_amedeo@yahoo.com>
2018-10-19 12:51:27 -0400

    Managed to obtain a 71% accuracy rate with a gradient boosting forest on a 2 way clasifier split 3:2, which is no bueno. Going to research peoples solutions to similar problems, or will have to find more data to make this model work.

--- Predict.py
+++ Predict.py
@@ -5,9 +5,17 @@ import pandas as pd
 from IPython import display
 import time
 from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import GridSearchCV
 
 #Obtain data from CSVs
-fundementalsData = pd.read_csv("./nyse/fundamentals.csv")
+fundementalsData = pd.read_csv("./nyse/fundamentals.csv").dropna()
 pricesData = pd.read_csv("./nyse/prices.csv")
 
 #Convert data to dataframes
@@ -44,3 +52,48 @@ mergeDataFrame["delta"] = mergeDataFrame["final"] - mergeDataFrame["initial"]
 
 #Merge previous data with fundementals data
 finalDataFrame = pd.merge(fundementalsDataFrame, mergeDataFrame, on=["symbol","date"])
+#display.display(finalDataFrame)
+
+finalDataFrame = pd.get_dummies(finalDataFrame)
+#display.display(finalDataFrame)
+
+finalDataFrame = finalDataFrame.drop(columns=['initial', 'final'])
+finalDataFrame = finalDataFrame.astype(np.float32)
+
+scaler = MinMaxScaler(feature_range=(-1,1))
+
+y_data = finalDataFrame['delta']
+y_data[y_data < 0] = 0
+y_data[y_data > 0] = 1
+print((y_data == 0).sum())
+print((y_data == 1).sum())
+y_data = y_data.astype(np.int64)
+x_data = finalDataFrame.drop(columns=['delta'])
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, random_state=0)
+
+grid = {
+    'max_depth': range(1,11),
+    'n_estimators': (10, 50, 100)
+}
+
+grid_svm = {
+    'C': np.logspace(-3, 3, num=7, base=10),
+    'gamma': np.logspace(-3, 3, num=7, base=10)
+}
+
+
+shuffle_split = ShuffleSplit(test_size=.8, train_size=.2, n_splits=5)
+
+grid_search_random = GridSearchCV(RandomForestClassifier(), grid, cv=shuffle_split)
+grid_search_gradient = GridSearchCV(GradientBoostingClassifier(), grid, cv=shuffle_split)
+grid_search_svm = GridSearchCV(SVC(), grid_svm, cv=5)
+grid_search_random.fit(x_train, y_train)
+grid_search_gradient.fit(x_train, y_train)
+grid_search_svm.fit(x_train, y_train)
+print("Random Forest:")
+print(grid_search_random.score(x_test, y_test))
+print("Gradient Forest:")
+print(grid_search_gradient.score(x_test, y_test))
+print("SVM:")
+print(grid_search_svm.score(x_test, y_test))
